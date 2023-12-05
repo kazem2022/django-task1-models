8693be23be8ae95320d8f03a48583fc0ae497b96
kazem <k_jebraeeli@yahoo.com>
2023-12-05 23:34:17 +0330

    pre-commit on categories models

--- categories/models.py
+++ categories/models.py
@@ -1,23 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(max_length=50)
-    
-    
+
     def __str__(self):
         return self.title
-    
+
+
 class Producer(models.Model):
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    
+
     def __str__(self):
         return self.title
-    
+
+
 class Product(models.Model):
-    producer = models.ForeignKey(Producer)
-    category = models.ForeignKey(Category)
+    producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     model = models.CharField(max_length=50)
     photo = models.ImageField()
