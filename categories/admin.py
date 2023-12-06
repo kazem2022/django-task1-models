de0d4370218ddbd6f4b8fed827732e090f55c4b1
kazem <k_jebraeeli@yahoo.com>
2023-12-06 15:38:36 +0330

    base-models-applied

--- categories/admin.py
+++ categories/admin.py
@@ -1,1 +1,7 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Category, Producer, Product
+
+admin.site.register(Category)
+admin.site.register(Producer)
+admin.site.register(Product)
