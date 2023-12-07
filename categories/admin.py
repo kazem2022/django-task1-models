d5ef1da32b1457b989ec450530c95e917983d909
kazem <k_jebraeeli@yahoo.com>
2023-12-07 11:31:41 +0330

    admin.py created

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Brand, Category, Product
+
+
+class BrandAdminInline(admin.TabularInline):
+    model = Brand
+    fields = ["name"]
+    extra = 0
+
+
+class ProductAdminInline(admin.TabularInline):
+    model = Product
+    fileds = ["name", "models", "price"]
+    extra = 0
+    inlines = [BrandAdminInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    inlines = [ProductAdminInline]
