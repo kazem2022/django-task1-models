70e4f0e5012ee72ac72c5e579bb8b6e1400527a9
kazem <k_jebraeeli@yahoo.com>
2023-12-07 11:01:20 +0330

    final-edit

--- categories/admin.py
+++ categories/admin.py
@@ -1,1 +1,22 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Brand, Category, Product
+
+
+class BrandAdminInline(admin.TabularInline):
+    model = Brand
+    fields = ["name"]
+    extra = 0
+
+
+class ProductAdminInline(admin.TabularInline):
+    model = Product
+    fileds = ["name", "models", "price", "is_enable"]
+    extra = 0
+    inlines = [BrandAdminInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    inlines = [ProductAdminInline]
--- categories/models.py
+++ categories/models.py
@@ -3,24 +3,28 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
-class Producer(models.Model):
+class Brand(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.title
+        return self.name
 
 
 class Product(models.Model):
-    producer = models.ForeignKey(Producer, on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     model = models.CharField(max_length=50)
     photo = models.ImageField()
     is_enable = models.BooleanField(default=False)
+    price = models.PositiveBigIntegerField(null=True)
+
+    def __str__(self):
+        return self.model
