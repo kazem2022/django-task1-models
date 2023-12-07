bab6acbf7d414b29a63d2c53c5bf5a81c4d70e19
kazem <k_jebraeeli@yahoo.com>
2023-12-07 09:53:37 +0330

    models applied to admin site

--- categories/admin.py
+++ categories/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 
-from .models import Category, Producer, Product
+from .models import Brand, Category, Product
 
-admin.site.register(Category)
-admin.site.register(Producer)
-admin.site.register(Product)
+
+class BrandAdminInline(admin.TabularInline):
+    model = Brand
+    fields = ["name"]
+    extra = 0
+
+
+class ProductAdminInline(admin.TabularInline):
+    model = Product
+    fileds = ["name", "models", "price"]
+    extra = 0
+    inlines = [BrandAdminInline]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    inlines = [ProductAdminInline]
